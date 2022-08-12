7f4a794aec67c2e4e330ca91737b1daa21c7979e
jasir <muhammedjasirmk01@gmail.com>
2022-08-12 14:43:18 +0530

    second commit

--- script.js
+++ script.js
@@ -3,13 +3,14 @@ let express = require('express');
 const cors = require('cors');
 const app = express();
 
-app.use(cors());
+
 
 app.get('/',(requset,response)=>{
     response.sendFile(__dirname+'/index.html');
     console.log('listening to port 3000');
 })
 
+app.use(cors());
 app.listen(3000);
 
 
